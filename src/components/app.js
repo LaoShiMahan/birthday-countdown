74f70ad9d6790104a88aa75e0b73db906327d497
Brodie Kelley <brodiekelley@hotmail.com>
2018-09-08 22:05:27 -0600

    Added header skew styles

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import BirthdayForm from './birthdayForm';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <BirthdayForm />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='app'>
+                <header>
+                    <h1>Birthday Countdown</h1>
+                    <div className='header__skew'>
+                        <div className='header__subskew'>
+                            <BirthdayForm />
+                        </div>
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
