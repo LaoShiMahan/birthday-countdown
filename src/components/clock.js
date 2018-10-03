b2be3047600e1bbc9ec19fea3e3f1beee37ba768
Brodie Kelley <brodiekelley@hotmail.com>
2018-10-03 13:13:17 -0600

    Refixed the bug in the getAge function

--- src/components/clock.js
+++ src/components/clock.js
@@ -72,7 +72,7 @@ class Clock extends Component {
         let today = new Date();
         let distance = today.getTime() - bday.getTime();
         let daysOld = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let yearsOld = Math.floor(Number(daysOld / 365));
+        let yearsOld = Math.ceil(Number(daysOld / 365));
         return yearsOld;
     }
 
